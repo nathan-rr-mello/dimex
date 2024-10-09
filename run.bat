2a0a67b6e4d6d4c3cc454923045ba0e4f72066f1
Gischenato <gischenato@gmail.com>
2024-10-09 00:31:53 -0300

    fix logs files

--- run.bat
+++ run.bat
@@ -4,6 +4,6 @@ del mxOUT.txt
 del snapshot0.txt
 del snapshot1.txt
 del snapshot2.txt
-start "0" cmd /k "go run useDIMEX-f.go 0 127.0.0.1:5000 127.0.0.1:6001 127.0.0.1:7002 > t3.txt"
-start "1" cmd /k "go run useDIMEX-f.go 1 127.0.0.1:5000 127.0.0.1:6001 127.0.0.1:7002 > t2.txt"
-start "2" cmd /k "go run useDIMEX-f.go 2 127.0.0.1:5000 127.0.0.1:6001 127.0.0.1:7002 > t1.txt"
+start "0" cmd /k "go run useDIMEX-f.go 0 127.0.0.1:5000 127.0.0.1:6001 127.0.0.1:7002 > t0.txt"
+start "1" cmd /k "go run useDIMEX-f.go 1 127.0.0.1:5000 127.0.0.1:6001 127.0.0.1:7002 > t1.txt"
+start "2" cmd /k "go run useDIMEX-f.go 2 127.0.0.1:5000 127.0.0.1:6001 127.0.0.1:7002 > t2.txt"
